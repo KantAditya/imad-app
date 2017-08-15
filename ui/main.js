b5c9755582099add4344b3ca806b8cf4852e2e9a
KantAditya <30632076+KantAditya@users.noreply.github.com>
2017-08-16 01:31:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'This is modified Web-App';
 //Move the image
 var img = document.getElementById('madi');
-img.onclick = function(){
+img.onclick = function() {
     img.style.marginLeft = '100px';
 };
