512a906d6cc3943aee2ee50af19f8f68f806b7b3
KantAditya <30632076+KantAditya@users.noreply.github.com>
2017-08-19 02:15:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,17 @@
 //first select the button by using getElementById
 var button = document.getElementById("counter");
-var counter = 0;
-//and when button is clicked a function is attached to it
-//what should this function do?
-//first make a request to counter end point
-//then capture the response and store it in a variable
-//then render the variable in correct span 
 button.onclick = function() {
-  counter = counter+1;
-  var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+      request.open('GET','http://kantaditya02.imad.hasura-app.io/counter',true);
+      request.send(null);
 };
