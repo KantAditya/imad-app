48f5e522edb940741a92ac4d1457b6ae07fc96fd
KantAditya <30632076+KantAditya@users.noreply.github.com>
2017-08-24 23:49:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ button.onclick = function() {
       request.send(null);
 };
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
         submit.onclick = function() {
                 var request = new XMLHttpRequest();
@@ -35,7 +34,8 @@ var submit = document.getElementById('submit_btn');
                         }
                     }
                 };
-                
+            var nameInput = document.getElementById('name');
+            var name = nameInput.value;
          request.open('GET','http://kantaditya02.imad.hasura-app.io/submit-name?name=' +name, true);
         request.send(null);        
         };
