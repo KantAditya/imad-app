093240e22e3e40e7191f222d55c121d74e9920d7
KantAditya <30632076+KantAditya@users.noreply.github.com>
2017-09-02 17:07:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
    
-   pool.query('SELECT * FROM "user" WHERE username = $1 AND password = $2', [username, password], function (err, result) {
+   pool.query('SELECT * FROM "user" WHERE username = $1 AND password = $2', [username, dbString], function (err, result) {
        if (err) {
             res.status(500).send(err.toString());
         } else {
